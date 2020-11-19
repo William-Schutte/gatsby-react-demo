19c9143eed01153c4d5629b8d7d1b005fa0cfacd
William Schutte <williamdschutte@gmail.com>
2020-11-18 20:14:47 -0500

    Use gatsby images for destination cards

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,12 +1,55 @@
 import React from 'react'
 import styled from 'styled-components'
-import {useStaticQuery, graphql} from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import {Button} from './Button'
+import {ImLocation} from 'react-icons/im'
 
-const Trips = () => {
+const Trips = ({heading}) => {
+  const data = useStaticQuery(graphql`
+    query TripsQuery {
+      allTripsJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getTrips(data) {
+    const tripsArray = [];
+    data.allTripsJson.edges.forEach((trip, index) => {
+      tripsArray.push(
+        <ProductCard key={index}>
+          <ProductImg src={trip.node.img.childImageSharp.fluid.src} fluid={trip.node.img.childImageSharp.fluid} alt={trip.node.alt} />
+          <ProductInfo>
+            <TextWrap>
+              <ImLocation />
+              <ProductTitle>{trip.node.name}</ProductTitle>
+            </TextWrap>
+            <Button to="/trips" primary="true" round="true" css={`position: absolute; top: 420px; font-size: 14px;`}>{trip.node.button}</Button>
+          </ProductInfo>
+        </ProductCard>
+      )
+    })
+    return tripsArray;
+  }
+
   return (
     <ProductsContainer>
-      <ProductsHeading>Heading</ProductsHeading>
-      <ProductsWrapper>Wrapper</ProductsWrapper>
+      <ProductsHeading>{heading}</ProductsHeading>
+      <ProductsWrapper>{getTrips(data)}</ProductsWrapper>
     </ProductsContainer>
   )
 }
@@ -24,4 +67,63 @@ const ProductsHeading = styled.div`
   margin-bottom: 5rem;
   color: #000;
 `
-const ProductsWrapper = styled.div``
+const ProductsWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+  justify-items: center;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ProductCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  position: relative;
+  border-radius: 10px;
+  transition: 0.2s ease;
+  overflow: hidden;
+`
+const ProductImg = styled(Img)`
+  height: 100%;
+  max-width: 100%;
+  position: absolute;
+  border-radius: 10px;
+  filter: brightness(60%);
+  transition: 1s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+  &:hover {
+    filter: brightness(100%);
+  }
+`
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 2rem;
+
+  @media screen and (max-width: 425px) {
+    padding: 0 1rem;
+  }
+`
+const TextWrap = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 375px;
+
+`
+
+const ProductTitle = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-left: 0.5rem;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <Hero />
-    <Trips />
+    <Trips heading="Explore More"/>
   </Layout>
 )
 
