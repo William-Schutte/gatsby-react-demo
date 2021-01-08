394f217d2bbf18882e44e567625482c3645b1c70
William Schutte <williamdschutte@gmail.com>
2021-01-08 14:40:09 -0500

    404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,36 @@
 import React from "react"
-
+import { Button } from '../components/Button'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from 'styled-components'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>I'm probably working on this page as we speak!</p>
+    <Error>
+      <h1>404</h1>
+      <p>Page not found! :(</p>
+      <Button to="/" round="true" big="true">Return Home</Button>
+    </Error>
   </Layout>
 )
 
 export default NotFoundPage
+
+const Error = styled.div`
+  background: #f26a2e;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-size: clamp(4rem, 10vw, 10rem);
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: clamp(2rem, 3vw, 10rem);
+    margin-bottom: 2rem;
+  }
+`
